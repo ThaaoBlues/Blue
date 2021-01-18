be9f0135db3307077966d5e938804ea7e8de0787
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2021-01-18 12:54:47 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from subprocess import Popen
 
 
 password = input("type your password : ")
-Popen(["echo",password,"|","sudo","apt","install","espeak"])
-Popen(["echo",password,"|","sudo","apt","install","alsa"])
-Popen(["echo",password,"|","sudo","apt","install","libasound-dev"])
-Popen(["echo",password,"|","sudo","apt","install","alsa-utils"])
-Popen(["echo",password,"|","sudo","apt","install","python3-pyaudio"])
+Popen(["echo",password,"|","sudo","apt","install","espeak"]).wait()
+Popen(["echo",password,"|","sudo","apt","install","alsa"]).wait()
+Popen(["echo",password,"|","sudo","apt","install","libasound-dev"]).wait()
+Popen(["echo",password,"|","sudo","apt","install","alsa-utils"]).wait()
+Popen(["echo",password,"|","sudo","apt","install","python3-pyaudio"]).wait()
 
-Popen(["python3","-m","pip","install","feedparser","youtube-dl","pafy","playsound","gTTS","GoogleNews","pywhatkit","SpeechRecognition","chatterbot==1.0.4","youtube_search","googletrans","colorama","speedtest-cli","wikipedia","google","googlesearch-python","flask","pyirobot","pyttsx3"])
+Popen(["python3","-m","pip","install","feedparser","youtube-dl","pafy","playsound","gTTS","GoogleNews","pywhatkit","SpeechRecognition","chatterbot==1.0.4","youtube_search","googletrans","colorama","speedtest-cli","wikipedia","google","googlesearch-python","flask","pyirobot","pyttsx3"]).wait()
 
 #custom files, can be modified by user on [blue IP]:8080
 open("custom_websites.blue","w")
