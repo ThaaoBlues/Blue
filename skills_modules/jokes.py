373808ac84a0b2cefba24ad26d341ba4af06b5c7
ThaaoBlues <40574784+ThaaoBlues@users.noreply.github.com>
2021-03-02 18:34:12 +0100

    Add files via upload

--- skills_modules/jokes.py
+++ skills_modules/jokes.py
@@ -4,5 +4,12 @@ from locale import getlocale
 
 def initialize(voice_command,sentences):
     j = Jokes()
-    return True,j.get_joke(blacklist=['racist'],lang=getlocale()[0][:2])
+    joke = j.get_joke(blacklist=['racist'],lang=getlocale()[0][:2])
+
+    if joke["type"] == "single":
+        return True, joke["joke"]
+
+    else:
+        return True, joke["setup"] + "."+joke["delivery"]
+
     
