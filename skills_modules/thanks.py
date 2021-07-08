899d6cdb20515738865f9c7a6c147acd867066d7
ThaaoBlues <thaaoblues81@gmail.com>
2021-07-08 20:43:56 +0200

    forgot to add args

--- skills_modules/thanks.py
+++ skills_modules/thanks.py
@@ -1,6 +1,6 @@
 from random import choice
 
 
-def initialize():
+def initialize(voice_command, sentences):
 
     return True, choice(["De rien mec !","c'est toi le boss !","avec plaisir !","votre magnificence chattoie même au delà de mes circuits !"])
