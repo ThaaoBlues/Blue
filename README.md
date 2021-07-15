a1e7b223cbd9166bee276366d454e798a0a069f3
ThaaoBlues <thaaoblues81@gmail.com>
2021-07-15 21:00:29 +0200

    made volume control cross platform

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This assistant is designed for french language, it will use a translator if you 
 
 - Linux (debian-based):
 ```bash
-sudo apt install python3 python3-pip python3-pyaudio git python3-dbus
+sudo apt install git python3 python3-pip python3-pyaudio python3-dbus python3-alsaaudio
 git clone https://www.github.com/ThaaoBlues/Blue
 cd ./Blue
 sudo python3 -m pip install -r requirements.txt
--- skills_modules/volume_down.py
+++ skills_modules/volume_down.py
@@ -1,11 +1,20 @@
-from pyautogui import press
+from platform import system
 
+try:
+    #windows
+    from pyautogui import press
+except ImportError:
+    #linux (debian-based)
+    from alsaaudio import Mixer
 
 
 def initialize(voice_command,sentences):
 
-
-    press('volumedown',presses=5)
-    
+    if system() == "Windows":
+        press('volumedown',presses=5)
+    else:
+        m = Mixer()
+        current_volume = m.getvolume() # Get the current Volume
+        m.setvolume(current_volume-10) # Set the volume to -10%.
 
     return True, ""
