41f5c52003cb4f0312c0ebed977905d061ff0707
ThaaoBlues <thaaoblues81@gmail.com>
2021-08-11 23:17:15 +0200

    disabled translator

--- util/translator.py
+++ util/translator.py
@@ -2,6 +2,15 @@ from googletrans import Translator
 
 
 def translate(text,src,dest='fr'):
+    """
+    sorry, this module is giving some errors,
+    using it is to unstable
+    
+    """
+    
     translator = Translator()
 
-    return translator.translate(text,src=src,dest=dest).text
+    #return translator.translate(text,src=src,dest=dest).text
+
+    return text
+
