58df429d242f8d2a5b405b3ffebd375c18376dac
ThaaoBlues <thaaoblues81@gmail.com>
2022-08-17 14:32:34 +0200

    fix

--- install_blue.sh
+++ install_blue.sh
@@ -1,13 +1,13 @@
 echo [!] Commands outputs will be stored in ./blue_install.log
 echo [+] Installing required packages via apt...
-sudo apt install git python3 python3-pip python3-tk python3-dev python3-pyaudio python3-dbus python3-alsaaudio vlc > ./blue_install.log
+sudo apt install git python3 python3-pip python3-tk python3-dev python3-pyaudio python3-dbus python3-alsaaudio vlc &> ./blue_install.log
 echo [v] Packages installed !
 echo [+] Cloning Blue from github...
-git clone https://www.github.com/ThaaoBlues/Blue > ./blue_install.log
+git clone https://www.github.com/ThaaoBlues/Blue &> ./blue_install.log
 echo [v] Blue cloned !
 echo [+] Cd into Blue folder
-cd ./Blue > ./blue_install.log
+cd ./Blue &> ./blue_install.log
 echo [+] Installing required python packages via pip... 
-python3 -m pip install -r linux_requirements.txt --no-cache-dir > ./blue_install.log
+python3 -m pip install -r linux_requirements.txt --no-cache-dir &> ./blue_install.log
 echo [v] Python packages installed, starting Blue !
 python3 Blue.py
