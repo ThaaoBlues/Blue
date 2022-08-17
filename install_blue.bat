08bbc0ecceeedee7420ef37a61eeebd2500d2df1
ThaaoBlues <thaaoblues81@gmail.com>
2022-08-17 21:43:06 +0200

    removed python installation from bat file

--- install_blue.bat
+++ install_blue.bat
@@ -1,10 +1,8 @@
-powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe -o python-installer.exe"
-start python-installer.exe
 powershell -Command "Invoke-WebRequest https://github.com/ThaaoBlues/Blue/archive/refs/heads/main.zip -o main.zip"
 powershell Expand-Archive main.zip -DestinationPath blue
 cd blue
 python -m pip install pipwin
 pipwin install pyaudio
 pipwin install psutil
-python -m pip install -r requirements.txt
+python -m pip install -r windows_requirements.txt
 python Blue.py
